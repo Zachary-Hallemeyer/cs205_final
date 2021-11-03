c21ed2726351f0c94464ba920dd96dbada4df15d
Zachary Hallemeyer <zhallemeyer@gmail.com>
2021-11-02 20:21:59 -0700

    Modified cs205_final_exam.sh and added functionality to file to print out number of pokemon, average HP, and average attack for the file passed into first parameter

--- cs205_final_exam.sh
+++ cs205_final_exam.sh
@@ -9,3 +9,16 @@
 # The spacing and header formatting should match the above formatting description exactly.
 # There should be a comment explaining the purpose of each line in your shell script. 
 # The data file will be passed in to the script as a positional parameter and will not necessarily be called pokemon.dat. However, you can assume that any file passed to this script will be formatted exactly the way pokemon.dat is formatted.
+
+#!/bin/bash
+
+FILE=$1;
+NUMBER_OF_POKEMON=$(awk 'BEGIN {sum=0}  {sum+=1} END {print sum-1}'  $FILE);
+TOTAL_HP=$(awk 'BEGIN {sum=0}  {sum+=$6} END {print sum}' $FILE);
+TOTAL_ATTACK=$(awk 'BEGIN {sum=0}  {sum+=$7} END {print sum}' $FILE);
+
+echo ======= SUMMARY OF POKEMON.DAT ======
+echo    Total Pokemon: $NUMBER_OF_POKEMON
+echo    Avg. HP: $(awk -v HP=$TOTAL_HP -v numPokemon=$NUMBER_OF_POKEMON 'BEGIN { print  ( HP / numPokemon ) }')
+echo    Avg. Attack: $(awk -v attack=$TOTAL_ATTACK -v numPokemon=$NUMBER_OF_POKEMON 'BEGIN { print  ( attack / numPokemon ) }')
+echo ======= END SUMMARY =======
